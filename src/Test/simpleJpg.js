a7223ee94caf3d851b67486e24966d7465fab343
Ratnesh-Dhan <ratneshdhan@gmail.com>
2024-12-03 23:50:55 +0530

    removed files

--- src/Test/simpleJpg.js
+++
@@ -1,9 +0,0 @@
-
-import Tesseract from 'tesseract.js';
-
-const worker = await Tesseract.createWorker();
-
-const {data: {text}} = await worker.recognize('./scan.jpg');
-console.log(text);
-
-await worker.terminate();
